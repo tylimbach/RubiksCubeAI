27a6c6f3188cb57b9da71d247abb80d10051f2a1
Tyler Limbach <tylimbach@gmail.com>
2021-11-23 21:17:17 -0600

    Added wide turns

--- C#App/CubeSolver2/CubeSolver/MenuLoop.cs
+++ C#App/CubeSolver2/CubeSolver/MenuLoop.cs
@@ -22,10 +22,10 @@ namespace CSharpApp
             var input = Console.ReadLine();
             _cube = new Cube(Convert.ToInt32(input));
 
-            while (input != "Q") 
+            while (input != "Q")
             {
                 _cube.Print2D();
-                
+
                 Console.Write("Enter a command: ");
                 input = Console.ReadLine();
 
@@ -52,66 +52,85 @@ namespace CSharpApp
                     Console.Clear();
                     continue;
                 }
-                
+
                 string[] parsedInput = input.Split(' ');
-                string command = parsedInput[0];
 
-                // determine if clockwise or ccw
-                bool clockwise = !command.EndsWith("'");
-                bool halfTurn = command.EndsWith("2");
-
-                Face face;
-                int offset;
-
-                // determine if slice turn and the offset layer
-                int idx = 0; // used to increment current character index
-                bool slice = char.IsNumber(command[0]);
-                if (slice)
+                for (int i = 0; i < parsedInput.Length; ++i)
                 {
-                    string offsetString = "";
-                    while (char.IsDigit(command[idx]))
+                    string command = parsedInput[i];
+
+                    // determine if clockwise or ccw
+                    bool clockwise = !command.EndsWith("'");
+                    bool halfTurn = command.EndsWith("2");
+
+                    Face face;
+                    int offset;
+
+                    // determine if slice turn and the offset layer
+                    int idx = 0; // used to increment current character index
+                    bool slice = char.IsNumber(command[0]);
+                    if (slice)
                     {
-                        offsetString += command[idx];
-                        ++idx;
+                        string offsetString = "";
+                        while (char.IsDigit(command[idx]))
+                        {
+                            offsetString += command[idx];
+                            ++idx;
+                        }
+
+                        offset = Int32.Parse(offsetString) - 1;
                     }
-                    offset = Int32.Parse(offsetString) - 1;
-                }
-                else
-                {
-                    offset = 0;
+                    else
+                    {
+                        offset = 0;
+                    }
+
+                    // Determine which character represents the face
+                    char faceChar = command[idx];
+
+                    // Determine which face to turn
+                    switch (faceChar)
+                    {
+                        case 'D':
+                            face = Face.Down;
+                            break;
+                        case 'F':
+                            face = Face.Front;
+                            break;
+                        case 'R':
+                            face = Face.Right;
+                            break;
+                        case 'U':
+                            face = Face.Up;
+                            break;
+                        case 'B':
+                            face = Face.Back;
+                            break;
+                        case 'L':
+                            face = Face.Left;
+                            break;
+                        default:
+                            continue;
+                    }
+                    
+                    // turn offset layer
+                    _cube.Turn(face, clockwise, offset);
+                    if (halfTurn) _cube.Turn(face, clockwise, offset);
+                    
+                    // turn layers between offset and face if wide turn
+                    if (command.Length > idx + 1)
+                    {
+                        if (command[idx + 1] == 'w')
+                        {
+                            for (int o = offset - 1; o >= 0; --o)
+                            {
+                                _cube.Turn(face, clockwise, o);
+                                if (halfTurn) _cube.Turn(face, clockwise, o);
+                            }
+                        }
+                    }
                 }
 
-                // Determine which character represents the face
-                char faceChar = command[idx];
-
-                // Determine which face to turn
-                switch(faceChar) 
-                {
-                    case 'D':
-                        face = Face.Down;
-                        break;
-                    case 'F':
-                        face = Face.Front;
-                        break;
-                    case 'R':
-                        face = Face.Right;
-                        break;
-                    case 'U':
-                        face = Face.Up;
-                        break;
-                    case 'B':
-                        face = Face.Back;
-                        break;
-                    case 'L':
-                        face = Face.Left;
-                        break;
-                    default:
-                        continue;
-                }
-                
-                _cube.Turn(face, clockwise, offset);
-                // turn again if half turn (180 degrees)
-                if (halfTurn) _cube.Turn(face, clockwise, offset);
             }
         }
     }
